d286b7d1d4278c999510908fed40ca59d6241510
sergio <sergiocorato@gmail.com>
2018-08-31 08:51:27 +0200

    [9.0] port invoice dueamount

--- account_invoice_dueamount/__openerp__.py
+++ account_invoice_dueamount/__openerp__.py
@@ -19,7 +19,7 @@
 #
 {
     'name': 'Account invoice due amount',
-    'version': '10.0.1.0.0',
+    'version': '9.0.1.0.0',
     'category': 'other',
     'author': 'Sergio Corato',
     'description': 'This module add optional custom due amount field, '
