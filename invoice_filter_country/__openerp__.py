23d3745e1dc451379a3087e409fe8929f251d6fb
sergio <sergiocorato@gmail.com>
2018-08-31 08:36:52 +0200

    [9.0] port filter country

--- invoice_filter_country/__openerp__.py
+++ invoice_filter_country/__openerp__.py
@@ -19,7 +19,7 @@
 #
 {
     'name': 'Invoice filter country',
-    'version': '10.0.1.0.0',
+    'version': '9.0.1.0.0',
     'category': 'other',
     'author': 'Sergio Corato',
     'description': 'Add country field to filter in invoice.',
